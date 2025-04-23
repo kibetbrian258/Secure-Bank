cb2a76c2ab23590f193a0263790955f50d6bdc4b
Brian Kibet <kibetbrian258@gmail.com>
2025-04-23 21:26:01 +0300

    Few changes

--- src/main/java/com/application/secureBank/Config/SecurityConfig.java
+++ src/main/java/com/application/secureBank/Config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://secure-bank-sb.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://secure-bank-sb.vercel.app", "https://matero-frontend.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
