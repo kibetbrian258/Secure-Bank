822ef67114c67d62f9949b6d62e65f330957aa97
Brian Kibet <kibetbrian258@gmail.com>
2025-04-25 10:56:08 -0700

    Built a Spring boot Jar file

--- src/main/java/com/application/secureBank/SecureBankApplication.java
+++ src/main/java/com/application/secureBank/SecureBankApplication.java
@@ -6,11 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SecureBankApplication extends SpringBootServletInitializer {
-
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SecureBankApplication.class);
-	}
+public class SecureBankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecureBankApplication.class, args);
